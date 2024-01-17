db3113469620829aae1d3ba66a9d962d2b39cf6b
sayam9901 <sayamkhan9560621912@gmail.com>
2024-01-17 19:45:11 +0530

    footer for the app

---
+++ src/app/Footer.tsx
@@ -0,0 +1,13 @@
+// components/Footer.tsx
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; 2024 Md Sayam's Blog. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
